bdd5fe739c29bc04cf4e1e2968533a7b2345541c
{M0G3K0} <{xx.M0G3K0.xx@gmail.com}>
2026-01-24 15:13:50 +0900

    chore: update knip config to ignore tool dependencies

--- knip.config.js
+++ knip.config.js
@@ -3,5 +3,15 @@ module.exports = {
 	entry: ['src/main.ts'],
 	project: ['src/**/*.ts'],
 	ignore: ['src/**/*.spec.ts'],
-	ignoreDependencies: ['@angular-devkit/build-angular', '@angular/cli', '@angular/compiler-cli', 'typescript'],
+	ignoreDependencies: [
+		'@angular-devkit/build-angular',
+		'@angular/cli',
+		'@angular/compiler-cli',
+		'typescript',
+		'eslint-plugin-tailwindcss',
+		'lint-staged',
+		'@tailwindcss/postcss',
+		'postcss',
+		'vitest',
+	],
 };
