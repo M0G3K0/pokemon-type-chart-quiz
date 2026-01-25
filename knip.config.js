6aad4bad34f1ead6334d39845b0980625380c24b
{M0G3K0} <{xx.M0G3K0.xx@gmail.com}>
2026-01-25 16:43:20 +0900

    fix: add token files to knip ignore (Issue #34)

--- knip.config.js
+++ knip.config.js
@@ -5,6 +5,9 @@ module.exports = {
 		'guards/**/*',
 		'src/app/domain/type-chart.ts',
 		'scripts/download-icons.js',
+		// TODO: Remove when first component imports tokens (see Issue #34)
+		'src/design-system/tokens/**/*',
+		'src/styles/generated/**/*',
 	],
 	ignoreDependencies: [
 		'@testing-library/angular',
